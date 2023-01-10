f6548fbcaddb7c691e7623d0633a061b1e5c649a
AntoineP2 <antoine.vinsonneau34@gmail.com>
2023-01-10 11:16:56 +0100

    all

--- components/nav.vue
+++ components/nav.vue
@@ -3,7 +3,7 @@
     <div class="flex justify-between p-0 m-0 h-12">
       <div>
         <v-tabs>
-          <NuxtLink to="/"><v-tab>One</v-tab></NuxtLink>
+          <NuxtLink to="/" ><v-tab>One</v-tab></NuxtLink>
           <NuxtLink to="/autrePage"><v-tab>Two</v-tab></NuxtLink>
           <v-tab>Three</v-tab>
           <v-tab>Four</v-tab>
@@ -15,11 +15,6 @@
 
 <script>
 export default {
-  data() {
-    return {
-      color: true,
-    };
-  },
 };
 </script>
 
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <v-card>
     <!-- <TemplatesNav @colorChoice="color"></TemplatesNav> -->
-    <Nav @colorChoice="color" />
+    <Nav />
   </v-card>
   <NuxtPage />
 </template>
