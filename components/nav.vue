19296d43f8ce47e69c8f0b6b78c869734a106ea4
a.vinsonneau <antoine.vinsonneau@compufirst.com>
2023-01-11 15:58:02 +0100

    nothing

--- components/nav.vue
+++ components/nav.vue
@@ -1,9 +1,18 @@
 <template>
   <v-card>
     <v-tabs fixed-tabs>
-      <v-tab to="/"> <Icon name ='octicon:home-fill-24' size="30px" /><p>Acceuil</p></v-tab>
-      <v-tab to="/autrePage"><Icon name ='ion:chatbubble-outline' size="30px" /><p>A propos</p></v-tab>
-      <v-tab><Icon name ='ion:ios-log-in' size="30px" /><p>Login</p></v-tab>
+      <v-tab to="/">
+        <Icon name="octicon:home-fill-24" size="30px" />
+        <p>Acceuil</p></v-tab
+      >
+      <v-tab to="/autrePage"
+        ><Icon name="ion:chatbubble-outline" size="30px" />
+        <p>A propos</p></v-tab
+      >
+      <v-tab
+        ><Icon name="ion:ios-log-in" size="30px" />
+        <p>Login</p></v-tab
+      >
     </v-tabs>
   </v-card>
 </template>
