bffdb45f4f61cc5230bb2da8a4d46209a910182c
AntoineP2 <antoine.vinsonneau34@gmail.com>
2023-03-21 11:38:20 +0100

    ssr false

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -39,14 +39,18 @@ export default {
     // },
 
     async login(pseudo, password) {
-      try {
         await store.login(pseudo, password);
-        this.tokenUser = store.$state.auth.data;
-        useCookie("tokenAuth", { maxAge: 30 }).value = this.tokenUser;
-        this.$router.push("/login/welcom");
-      } catch (e) {
-        console.log(e);
-      }
+        if(store.$state.auth.data != null){
+            this.tokenUser = store.$state.auth.data;
+            useCookie("tokenAuth", { maxAge: 30 }).value = this.tokenUser;
+            this.$router.push("/login/welcom");
+        }
+        else {
+            console.log("Error dude")
+        }
+        
+        
+        
       // Cette méthode renvoie true ou false selon si l'utilisateur est authentifié ou non
     },
 
