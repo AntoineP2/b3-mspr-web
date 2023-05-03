1e0eaa62fdabb03ce2de7f018866b5ca2b0961f9
a.vinsonneau <antoine.vinsonneau@compufirst.com>
2023-05-03 09:03:41 +0200

    ok

--- components/specieSheet.vue
+++ components/specieSheet.vue
@@ -1,38 +1,27 @@
 <template>
-    <v-card
-    class="mt-2"
-    variant = "tonal"
-  >
-    <v-img
-      class="align-end text-white"
-      height="200"
-      :src="specie.img"
-      cover
-    >
+  <v-card class="mt-2" variant="tonal">
+    <v-img class="align-end text-white" height="200" :src="specie.img" cover>
+      <v-card-title> {{ specie.name }}</v-card-title>
     </v-img>
-    
-      <v-card-actions>
-        <v-card-subtitle> {{ specie.name }}</v-card-subtitle>
+
+    <v-card-actions>
       <v-btn color="green" @click="$emit('goTo', specie.ID)">
         Voir les conseils
       </v-btn>
     </v-card-actions>
-
   </v-card>
 </template>
 
 <script>
 export default {
-    props : {
-        specie : {
-            type : Object,
-            required : true
-        }
-    }
-
-}
+  props: {
+    specie: {
+      type: Object,
+      required: true,
+    },
+  },
+};
 </script>
 
 <style>
-
 </style>
