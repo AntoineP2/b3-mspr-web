726812b0de1b28336ebbf72e423adc4361b992d1
a.vinsonneau <antoine.vinsonneau@compufirst.com>
2023-05-03 09:50:56 +0200

    ok

--- components/specieSheet.vue
+++ components/specieSheet.vue
@@ -1,6 +1,11 @@
 <template>
-  <v-card class="mt-2" variant="tonal">
-    <v-img class="align-end text-white" height="200" :src="specie.img" cover>
+  <v-card class="mt-2 hover:scale-105" variant="tonal">
+    <v-img
+      class="align-end text-white filter brightness-50 hover:brightness-100 transition duration-150 ease-in-out"
+      height="200"
+      :src="specie.img"
+      cover
+    >
       <v-card-title> {{ specie.name }}</v-card-title>
     </v-img>
 
